db3e80bfaba06415a610564821858f5ecf9b2abd
Alex Safatli <safatli@cs.dal.ca>
2015-10-22 09:43:00 -0300

    Correct for new Holder syntax
    How does this look? It should work if my experimentation is any indication. I've also added another argument for specific `Holder.js` options (see [here](https://github.com/imsky/holder))

--- lib/holder_rails/helpers.rb
+++ lib/holder_rails/helpers.rb
@@ -1,9 +1,12 @@
 module HolderRails
   module Helpers
-    def holder_tag(size, text='', theme=nil, html_options={})
+    def holder_tag(size, text='', theme=nil, html_options={}, holder_options={})
       size = "#{size}x#{size}" unless size =~ /\A\d+%?x\d+\z/
-      text = text.to_s.empty? ? size : text
-      options = {:src => '', :data => {:src => "holder.js/#{size}/text:#{text}/#{theme}"}}
+      holder = "holder.js/#{size}?text=#{text}&theme=#{theme}"
+      holder_options.each do |key, value|
+        holder += "&#{key}=#{value}"
+      end
+      options = {:src => '', :data => {:src => holder}}
       options = options.merge(html_options)
 
       tag :img, options
