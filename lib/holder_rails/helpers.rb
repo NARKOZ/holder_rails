b18d26abc967e11cbe3b4f8e4bd7f13d46e913a8
Nihad Abbasov <narkoz.2008@gmail.com>
2015-11-24 14:23:51 +0400

    fix placeholder options

--- lib/holder_rails/helpers.rb
+++ lib/holder_rails/helpers.rb
@@ -1,9 +1,13 @@
 module HolderRails
   module Helpers
-    def holder_tag(size, text='', theme=nil, html_options={})
+    def holder_tag(size, text='', theme=nil, html_options={}, holder_options={})
       size = "#{size}x#{size}" unless size =~ /\A\d+%?x\d+\z/
-      text = text.to_s.empty? ? size : text
-      options = {:src => '', :data => {:src => "holder.js/#{size}/text:#{text}/#{theme}"}}
+
+      holder_options[:text] = text unless text.to_s.empty?
+      holder_options[:theme] = theme unless theme.nil?
+      holder_options = holder_options.map {|e| e.join('=') }.join('&')
+
+      options = { src: '', data: { src: "holder.js/#{size}?#{holder_options}" }}
       options = options.merge(html_options)
 
       tag :img, options
--- test/holder_rails_test.rb
+++ test/holder_rails_test.rb
@@ -5,21 +5,26 @@ class HolderRailsTest < ActionView::TestCase
   include HolderRails::Helpers
 
   test "size" do
-    assert_dom_equal '<img data-src="holder.js/100x100/text:100x100/" src="" />', holder_tag(100)
-    assert_dom_equal '<img data-src="holder.js/200x300/text:200x300/" src="" />', holder_tag('200x300')
-    assert_dom_equal '<img data-src="holder.js/100%x75/text:100%x75/" src="" />', holder_tag('100%x75')
+    assert_dom_equal '<img data-src="holder.js/100x100?" src="" />', holder_tag(100)
+    assert_dom_equal '<img data-src="holder.js/200x300?" src="" />', holder_tag('200x300')
+    assert_dom_equal '<img data-src="holder.js/100%x75?" src="" />', holder_tag('100%x75')
   end
 
   test "text" do
-    assert_dom_equal '<img data-src="holder.js/200x300/text:Lorem ipsum/" src="" />', holder_tag('200x300', 'Lorem ipsum')
+    assert_dom_equal '<img data-src="holder.js/200x300?text=Lorem ipsum" src="" />', holder_tag('200x300', 'Lorem ipsum')
   end
 
   test "theme" do
-    assert_dom_equal '<img data-src="holder.js/200x300/text:Lorem ipsum/social" src="" />', holder_tag('200x300', 'Lorem ipsum', 'social')
+    assert_dom_equal '<img data-src="holder.js/200x300?text=Lorem ipsum&amp;theme=social" src="" />', holder_tag('200x300', 'Lorem ipsum', 'social')
   end
 
   test "html_options" do
-    assert_dom_equal '<img class="special" data-src="holder.js/500x800/text:Example text/gray" id="new" src="" />',
-      holder_tag('500x800', 'Example text', 'gray', :id => 'new', :class => 'special')
+    assert_dom_equal '<img class="special" data-src="holder.js/500x800?text=Example text&amp;theme=gray" id="new" src="" />',
+      holder_tag('500x800', 'Example text', 'gray', id: 'new', class: 'special')
+  end
+
+  test "holder_options" do
+    assert_dom_equal '<img class="special" data-src="holder.js/500x800?font=Helvetica&amp;text=Example text&amp;theme=gray" id="new" src="" />',
+      holder_tag('500x800', 'Example text', 'gray', { id: 'new', class: 'special' }, { font: 'Helvetica' })
   end
 end
